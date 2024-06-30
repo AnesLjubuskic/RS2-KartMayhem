0b773c7f75e8c26be53dd98f0e36a4955a8caa4d
AnesLjubuskic <anes3_it@hotmail.com>
2024-06-30 03:06:26 +0200

    feedbackscreen

--- UI/kartmayhem_mobile/lib/Screens/feedback_screen.dart
+++ UI/kartmayhem_mobile/lib/Screens/feedback_screen.dart
@@ -8,6 +8,9 @@ class FeedbackScreen extends StatefulWidget {
 }
 
 class _FeedbackScreenState extends State<FeedbackScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _feedback = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,18 +20,62 @@ class _FeedbackScreenState extends State<FeedbackScreen> {
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Column(
-        children: const [
-          Center(
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            const Center(
               child: Text(
                 "Ostavite komentar o aplikaciji",
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            Form(
+              key: _formKey,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Unesite vaš feedback',
+                  border: OutlineInputBorder(),
+                ),
+                maxLength: 150,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Feedback ne može biti prazan';
+                  }
+                  if (value.length > 150) {
+                    return 'Feedback mora biti ispod 150 karaktera';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _feedback = value!;
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  // Process the feedback (e.g., send it to a server)
+                  print('Feedback: $_feedback');
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF870000),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 10.0, 20, 10.0),
+                child: Text('Spasi',
+                    style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
