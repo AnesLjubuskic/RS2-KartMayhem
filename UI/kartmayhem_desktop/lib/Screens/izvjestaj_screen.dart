caf885c95643e0248efbf329b5662e260acdf897
AnesLjubuskic <anes3_it@hotmail.com>
2024-07-06 17:19:16 +0200

    download pdf

--- UI/kartmayhem_desktop/lib/Screens/izvjestaj_screen.dart
+++ UI/kartmayhem_desktop/lib/Screens/izvjestaj_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:kartmayhem_desktop/Models/izvjestaji.dart';
 import 'package:kartmayhem_desktop/Models/search_result.dart';
@@ -10,6 +12,9 @@ import 'package:kartmayhem_desktop/Screens/nagradi_screen.dart';
 import 'package:kartmayhem_desktop/Screens/rezervacije_screen.dart';
 import 'package:kartmayhem_desktop/Screens/sidebar_navigation.dart';
 import 'package:kartmayhem_desktop/Screens/staze_screen.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path_provider/path_provider.dart';
 
 class IzvjestajScreen extends StatefulWidget {
   static const String routeName = '/izvjestaj';
@@ -152,21 +157,19 @@ class _IzvjestajScreenState extends State<IzvjestajScreen> {
                       ],
                     ),
                     const SizedBox(height: 50),
-                    Container(
-                      child: Align(
-                        alignment: Alignment.topCenter,
-                        child: (_selectedStaza == "Sve")
-                            ? Text(
-                                "Statistika za sve staze u $_selectedYear godini:",
-                                style: const TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
-                              )
-                            : Text(
-                                "Statistika za ${result!.result.where((x) => x.id.toString() == _selectedStaza).first.nazivStaze} u $_selectedYear godini:",
-                                style: const TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
-                              ),
-                      ),
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: (_selectedStaza == "Sve")
+                          ? Text(
+                              "Statistika za sve staze u $_selectedYear godini:",
+                              style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            )
+                          : Text(
+                              "Statistika za ${result!.result.where((x) => x.id.toString() == _selectedStaza).first.nazivStaze} u $_selectedYear godini:",
+                              style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
                     ),
                     Align(
                       alignment: Alignment.topCenter,
@@ -179,18 +182,18 @@ class _IzvjestajScreenState extends State<IzvjestajScreen> {
                     Align(
                       alignment: Alignment.topCenter,
                       child: Text(
-                        "Ukupna zarada: ${izvjestaj?.ukupnaZaradaStaze}",
+                        "Ukupna zarada: ${izvjestaj?.ukupnaZaradaStaze}KM",
                         style: const TextStyle(
                             fontSize: 20, fontWeight: FontWeight.normal),
                       ),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 20,
                     ),
                     const Align(
                       alignment: Alignment.topCenter,
                       child: Text(
-                        "Ostatala statistika",
+                        "Ostala statistika",
                         style: TextStyle(
                             fontSize: 20, fontWeight: FontWeight.bold),
                       ),
@@ -206,7 +209,7 @@ class _IzvjestajScreenState extends State<IzvjestajScreen> {
                     Align(
                       alignment: Alignment.topCenter,
                       child: Text(
-                        "Ukupna zarada kroz aplikaciju: ${izvjestaj?.ukupnaZaradaAplikacije}",
+                        "Ukupna zarada kroz aplikaciju: ${izvjestaj?.ukupnaZaradaAplikacije}KM",
                         style: const TextStyle(
                             fontSize: 20, fontWeight: FontWeight.normal),
                       ),
@@ -222,6 +225,51 @@ class _IzvjestajScreenState extends State<IzvjestajScreen> {
     );
   }
 
+  Future<void> _pdfGenerator() async {
+    final pdf = pw.Document();
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Column(
+            crossAxisAlignment: pw.CrossAxisAlignment.start,
+            children: [
+              (_selectedStaza == "Sve")
+                  ? pw.Text(
+                      "Statistika za sve staze u $_selectedYear godini:",
+                      style: const pw.TextStyle(fontSize: 20),
+                    )
+                  : pw.Text(
+                      "Statistika za ${result!.result.where((x) => x.id.toString() == _selectedStaza).first.nazivStaze} u $_selectedYear godini:",
+                      style: const pw.TextStyle(fontSize: 20),
+                    ),
+              pw.SizedBox(height: 20),
+              pw.Text('Broj rezervacija: ${izvjestaj?.brojRezervacijeStaze}'),
+              pw.Text('Ukupna zarada: ${izvjestaj?.ukupnaZaradaStaze}KM'),
+              pw.SizedBox(height: 20),
+              pw.Text('Ostatala statistika',
+                  style: const pw.TextStyle(fontSize: 20)),
+              pw.SizedBox(height: 20),
+              pw.Text(
+                  'Ukupan broj korisnika aplikacije: ${izvjestaj?.ukupanBrojKorisnikaAplikacije}'),
+              pw.Text(
+                  'Ukupna zarada kroz aplikaciju: ${izvjestaj?.ukupnaZaradaAplikacije}KM'),
+            ],
+          );
+        },
+      ),
+    );
+    final directory = await getApplicationDocumentsDirectory();
+    final timestamp = DateTime.now().millisecondsSinceEpoch;
+    final path = "${directory.path}/izvjestaj_$timestamp.pdf";
+    final File file = File(path);
+    await file.writeAsBytes(await pdf.save());
+
+    // ignore: use_build_context_synchronously
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Izvještaj spremljen na lokaciji $path')),
+    );
+  }
+
   Center pdfButton() {
     return Center(
       child: Padding(
@@ -231,7 +279,9 @@ class _IzvjestajScreenState extends State<IzvjestajScreen> {
               backgroundColor: const Color(0xFF870000),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10))),
-          onPressed: () {},
+          onPressed: () async {
+            await _pdfGenerator();
+          },
           child: const Padding(
             padding: EdgeInsets.all(10.0),
             child: Text(
